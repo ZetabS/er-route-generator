1b197d0a93aef93d77063628e127a268065bbac5
Zetab_S <songkm2@naver.com>
2023-10-22 23:33:05 +0900

    Fix weapon type img
    무기 종류 이미지가 번경되지 않는 버그 수정

--- src/components/icon/WeaponTypeIcon.vue
+++ src/components/icon/WeaponTypeIcon.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { ref, watch } from 'vue';
 const { weaponType, width, height } = defineProps(['weaponType', 'width', 'height']);
 
-const imgPath = computed(() => {
-  return import.meta.env.BASE_URL + `images/weapon-type/${weaponType}.png`;
+const imgPath = ref('');
+
+watch(imgPath, () => {
+  return `/er-route-generator/images/weapon-type/${weaponType}.png`;
 });
 </script>
 
 <template>
-  <img :src="imgPath" :style="`width: ${width}; height: ${height}; `" alt="Weapon" />
+  <img
+    :src="`/er-route-generator/images/weapon-type/${weaponType}.png`"
+    :style="`width: ${width}; height: ${height}; `"
+    alt="Weapon"
+  />
 </template>
 
 <style scoped></style>
