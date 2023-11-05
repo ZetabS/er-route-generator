85aa2384b5a6326568408cf62e6b28b57603b61a
Zetab_S <songkm2@naver.com>
2023-11-06 05:57:58 +0900

    test: TestComponent

--- src/components/sidebar/test/TestComponent.vue
+++ src/components/sidebar/test/TestComponent.vue
@@ -1,41 +1,33 @@
 <script setup lang="ts">
-import { computed, ComputedRef, Ref, ref } from 'vue';
+// import TestText from './TestText.vue';
+import { computed } from 'vue';
 import SidebarWrapper from '@/components/wrapper/SidebarWrapper.vue';
-import TestText from './TestText.vue';
-import {
-  getAreaByCode,
-  getItemByCode,
-  getRecipeByMaterial,
-  koreanData
-} from '@/modules/api/apiService';
 import { useSelectedStore } from '@/stores/selected';
+import { Plan } from '@/modules/plan';
+import ItemIcon from '@/components/icon/ItemIcon.vue';
+import { AREA } from '@/modules/api';
 
 const selected = useSelectedStore();
-
-import { findMaterialsInArea, getAllMaterials } from '@/modules/plan/Plan';
-import type { Item } from '@/common/typing';
-import ItemIcon from '@/components/icon/ItemIcon.vue';
-import { recipeData } from '@/modules/api/data/recipeData';
-
-const materials = computed(() => getAllMaterials(selected.itemsArray));
-
-const materialsName = computed(() => materials.value.map((item) => item.name));
-const recipes = computed(() =>
-  getRecipeByMaterial(203102).map((recipe) => getItemByCode(recipe.itemCode).name)
-);
-const areaItems = computed(() => findMaterialsInArea(getAreaByCode(12), selected.itemsArray));
-const areaItemsName = computed(() => areaItems.value.map((item) => item.name));
-const num = ref(0);
+const plan = computed(() => {
+  return new Plan([AREA['병원'], AREA['고급 주택가'], AREA['숲']], selected.itemsArray);
+});
 </script>
 
 <template>
   <SidebarWrapper :size="10">
-    {{ areaItemsName }}
-    <item-icon v-for="item of areaItems" :key="item" :item="item" :size="3" />
+    <div class="container" v-for="num of [1, 2, 3]" :key="num">
+      {{ plan.inventoryAt(num).toString()[0] }}
+      {{ plan.inventoryAt(num).toString()[1] }}
+      <item-icon v-for="item of plan.inventoryAt(num).items" :key="item" :item="item" :size="3" />
+    </div>
   </SidebarWrapper>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+}
+
 button {
   width: 5rem;
   height: 5rem;
