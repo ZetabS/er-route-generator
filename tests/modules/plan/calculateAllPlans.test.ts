f56aa4c2a8477a79b41fc2f7beb6b5dda5c4c70d
Zetab_S <songkm2@naver.com>
2023-11-13 12:30:58 +0900

    test: change profile directory

--- tests/modules/plan/calculateAllPlans.test.ts
+++ tests/modules/plan/calculateAllPlans.test.ts
@@ -1,8 +1,8 @@
-import v8Profiler from 'v8-profiler-next';
 import { calculateAllPlans } from '@/modules/plan/calculateAllPlans';
 import { Item, ITEM_BY_NAME } from '@/modules/api';
 import { Plan } from '@/modules/plan';
 import * as fs from 'fs';
+import v8Profiler from 'v8-profiler-next';
 
 v8Profiler.setGenerateType(1);
 const title = 'calculate-profile';
@@ -12,17 +12,11 @@ describe('calculateAllRoutes', () => {
   afterAll(() => {
     const profile = v8Profiler.stopProfiling(title);
     profile.export(function (error, result: any) {
-      // if it doesn't have the extension .cpuprofile then
-      // chrome's profiler tool won't like it.
-      // examine the profile:
-      //   Navigate to chrome://inspect
-      //   Click Open dedicated DevTools for Node
-      //   Select the profiler tab
-      //   Load your file
-      fs.writeFileSync(`${title}.cpuprofile`, result);
+      fs.writeFileSync(`cpuprofile/${title}.cpuprofile`, result);
       profile.delete();
     });
   });
+
   const targetItems: Item[] = [
     ITEM_BY_NAME['활빈검'],
     ITEM_BY_NAME['지휘관의 갑옷'],
@@ -33,8 +27,5 @@ describe('calculateAllRoutes', () => {
 
   test('calculateAllRoutes', () => {
     const plans: Plan[] = calculateAllPlans(targetItems);
-    for (const plan of plans) {
-      console.log(plan.route.map((area) => area.name));
-    }
   });
 });
