d937ffedeab2906f2ea9f1c628468801213eb9d0
Zetab_S <songkm2@naver.com>
2023-11-13 11:09:43 +0900

    chore: add v8-profiler-next

--- tests/modules/plan/calculateAllPlans.test.ts
+++ tests/modules/plan/calculateAllPlans.test.ts
@@ -1,8 +1,28 @@
+import v8Profiler from 'v8-profiler-next';
 import { calculateAllPlans } from '@/modules/plan/calculateAllPlans';
 import { Item, ITEM_BY_NAME } from '@/modules/api';
 import { Plan } from '@/modules/plan';
+import * as fs from 'fs';
+
+v8Profiler.setGenerateType(1);
+const title = 'calculate-profile';
 
 describe('calculateAllRoutes', () => {
+  v8Profiler.startProfiling(title, true);
+  afterAll(() => {
+    const profile = v8Profiler.stopProfiling(title);
+    profile.export(function (error, result: any) {
+      // if it doesn't have the extension .cpuprofile then
+      // chrome's profiler tool won't like it.
+      // examine the profile:
+      //   Navigate to chrome://inspect
+      //   Click Open dedicated DevTools for Node
+      //   Select the profiler tab
+      //   Load your file
+      fs.writeFileSync(`${title}.cpuprofile`, result);
+      profile.delete();
+    });
+  });
   const targetItems: Item[] = [
     ITEM_BY_NAME['활빈검'],
     ITEM_BY_NAME['지휘관의 갑옷'],
